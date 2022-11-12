26ca0111150af5d7c61a931b2c4102bf0b989963
ottojoash <louisjoshbricks@gmail.com>
2022-11-12 06:38:45 -0500

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(App());
 }
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Homepage(),
+    );
+  }
+}
+
