5a073349016be0939de93d4a5ff7bc7ae4368bda
ottojoash <louisjoshbricks@gmail.com>
2022-11-12 06:49:31 -0500

    home state

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,7 @@ class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Homepage(),
-    );
-  }
+  HomePageState createState() => 
+  _HomePageState();
 }
 
