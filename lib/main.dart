6dacc231fa6750ec6db697e5032f3504af5342af
ottojoash <louisjoshbricks@gmail.com>
2022-11-21 02:34:14 -0500

    home

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'dart:html';
+import 'dart:io';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutterwave/flutterwave.dart';
+import 'package:flutterwave/models/responses/charge_response.dart';
 
 void main() {
   runApp(App());
@@ -11,10 +13,21 @@ class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
-  HomePageState createState() => _HomePageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
 }
 
-class _HomePageState extends State<Homepage> {
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   TextEditingController _email = TextEditingController();
   TextEditingController _amount = TextEditingController();
 
@@ -26,7 +39,7 @@ class _HomePageState extends State<Homepage> {
 
     if (Platform.isAndroid) {
       setState(() {
-        _ref = "AndriodRef1789$number";
+        _ref = "AndroidRef1789$number";
       });
     } else {
       setState(() {
@@ -34,42 +47,121 @@ class _HomePageState extends State<Homepage> {
       });
     }
   }
-}
 
-@override
-void initState() {
-  setRef();
-  super.initState();
-}
+  @override
+  void initState() {
+    setRef();
+    super.initState();
+  }
 
-@override 
-Widget build(BuildContext context){
-  return Scaffold(
-    appBar: AppBar(title: Text("Flutter WAve Appplication")),
-    body: Stack(children: [
-      Container(
-        margin: const EdgeInsets.only(
-          bottom: 10),
-          child: TextFormField(
-            controller: _email,
-            decoration: const InputDecoration(
-              labelText: "Email"),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Flutter Wave Application")),
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(bottom: 10),
+                  child: TextFormField(
+                    controller: _email,
+                    decoration: InputDecoration(labelText: "Email"),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 10),
+                  child: TextFormField(
+                    controller: _amount,
+                    decoration: InputDecoration(labelText: "Amount"),
+                  ),
+                ),
+              ],
             ),
           ),
-          Container(
-            margin: const EdgeInsets.only(
-              bottom: 10),
-              child: TextFormField(
-                controller: _amount,
-                decoration: const InputDecoration(
-                  labelText: "Amount"),
-                ),
+
+          ///Button
+          Positioned(
+            bottom: 0,
+            child: GestureDetector(
+              onTap: () {
+                final email = _email.text;
+                final amount = _amount.text;
+
+                if (email.isEmpty || amount.isEmpty) {
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text("Field are empty")));
+                } else {
+                  ///Flutterwave Payment
+                  _makePayment(context, email.trim(), amount.trim());
+                }
+              },
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                width: MediaQuery.of(context).size.width,
+                color: Colors.lightBlue,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ///Icon
+                      Icon(Icons.payment),
+
+                      Text(
+                        "Make Payment",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ]),
               ),
-    
             ),
           )
-        ),
-      )
-    ]),
-  )
+        ],
+      ),
+    );
+  }
+
+  void _makePayment(BuildContext context, String email, String amount) async {
+    try {
+      Flutterwave flutterwave = Flutterwave.forUIPayment(
+          context: this.context,
+          encryptionKey: "FLWSECK_TEST4734027149f5",
+          publicKey: "FLWPUBK_TEST-e8d4cd5462fb381c4163ba7f7f746a98-X",
+          currency: "NGN",
+          amount: amount,
+          email: "$email",
+          fullName: "Destiny Ed",
+          txRef: _ref!,
+          isDebugMode: true,
+          phoneNumber: "0123456789",
+          acceptCardPayment: true,
+          acceptUSSDPayment: true,
+          acceptAccountPayment: true,
+          acceptFrancophoneMobileMoney: false,
+          acceptGhanaPayment: false,
+          acceptMpesaPayment: true,
+          acceptRwandaMoneyPayment: false,
+          acceptUgandaPayment: false,
+          acceptZambiaPayment: false);
+
+      final ChargeResponse response =
+          await flutterwave.initializeForUiPayments();
+
+      if (response == null) {
+        print("Transaction Failed");
+      } else {
+        ///
+        if (response.status == "success") {
+          print(response.data);
+          print(response.message);
+
+        } else {
+          print(response.message);
+        }
+      }
+    } catch (error) {
+      print(error);
+    }
+  }
 }
